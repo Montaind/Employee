a9b56977dfb0031bdbb12a19ff14cd37b4ad3095
Богдан Грибачев <banlypro10@gmail.com>
2023-11-26 19:46:10 +0300

    Спасибо!

--- Employee/main.cpp
+++ Employee/main.cpp
@@ -4,15 +4,17 @@
 #include <vector>
 
 int main() {
-	Employee** a = new Employee*[3];
+	std::vector<Employee*> employee;
 	Worker A("Grigoriy", 5, "woman", 0, 24, 1);
 	Worker B("Ilya", 14, "man", 8, 10, 12);
 	Manager C("Bogdan", 14, "man", SupremeManager);
-	a[0] = &A;
-	a[1] = &B;
-	a[2] = &C;
-	for (int i = 0; i < 3; i++) {
-		a[i]->output();
+
+	employee.push_back(&A);
+	employee.push_back(&B);
+	employee.push_back(&C);
+
+	for (int i = 0; i < employee.size(); i++) {
+		employee[i]->output();
 		std::cout << "\n\n\n";
 	}
 }
